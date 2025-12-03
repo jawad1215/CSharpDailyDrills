0fe98648ad779ca038a1c2528686098de0747636
jawad <jawadyousaf37@gmail.com>
2025-12-03 21:20:21 +0500

    Day 1 Done

---
+++ Day01/Program.cs
@@ -0,0 +1,15 @@
+﻿class Day01
+{
+    static void Main()
+    {
+        int age = 25;
+        double height = 5.7;
+        bool isStudent = false;
+        string name = "Jawad";
+        
+        Console.WriteLine("Name: "+name);
+        Console.WriteLine("Height: " +height);
+        Console.WriteLine("IsStudent: "+isStudent);
+        Console.WriteLine("Age: "+age);
+    }
+}
---
+++ Day01_Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Day01_Exercise;
+
+class Day01_Exercise
+{
+    private static void Main()
+    {
+        const string name = "Jawad";
+        const int age = 30;
+        const double currentSalary = 150;
+        const string country = "Pakistan";
+        const bool isLearning = true;
+        
+        Console.WriteLine($"{name} {age} {country} {isLearning}");
+        
+        const double futureSalary = 300;
+        
+        const bool isGoalAchievable = currentSalary < futureSalary;
+        Console.WriteLine(isGoalAchievable);
+
+
+        const int number1 = 150;
+        const int number2 = 50;
+        
+        const int sum = number1 + number2;
+        const int subtract = number1 - number2;
+        const int multiply = number1 * number2;
+        const int divide = number1 / number2;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Subtract: " + subtract);
+        Console.WriteLine("Multiply: " + multiply);
+        Console.WriteLine("Divide: " + divide);
+
+    }
+}
